480b35101a798d86740d868b9911652714e3448e
Sam Ramezanli <samramezz@gmail.com>
2014-10-23 15:05:05 -0400

    Animation working
    Animation works with some sample data.
    No database yet.

--- app/src/main/java/com/example/samramez/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/samramez/shoppinglist/MainActivity.java
@@ -2,16 +2,22 @@ package com.example.samramez.shoppinglist;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    Button addButton;
-    EditText addItem;
+    private Button addButton;
+    private EditText addItem;
+    private ViewGroup mContainerView;
 
 
     @Override
@@ -22,7 +28,61 @@ public class MainActivity extends ActionBarActivity {
         //initiating page elements
         addButton = (Button) findViewById(R.id.addButton);
         addItem = (EditText) findViewById(R.id.addItemEditText);
+        mContainerView = (ViewGroup) findViewById(R.id.container);
+
+        addButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(android.R.id.empty).setVisibility(View.GONE);
+                addItem();
+            }
+        });
     }
+
+    // a method to inflate item to the main activity.
+    private void addItem() {
+
+        // Instantiate a new "row" view.
+        final ViewGroup newView = (ViewGroup) LayoutInflater.from(this).inflate(
+                R.layout.list_item_inflate, mContainerView, false);
+
+
+
+
+        // Set the text in the new row to a random item.
+        ((TextView) newView.findViewById(android.R.id.text1)).setText(
+                ITEMS[(int) (Math.random() * ITEMS.length)]);
+
+        // Set a click listener for the "X" button in the row that will remove the row.
+        newView.findViewById(R.id.delete_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Remove the row from its parent (the container view).
+                // Because mContainerView has android:animateLayoutChanges set to true,
+                // this removal is automatically animated.
+                mContainerView.removeView(newView);
+
+                // If there are no rows remaining, show the empty view.
+                if (mContainerView.getChildCount() == 0) {
+                    findViewById(android.R.id.empty).setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        // Because mContainerView has android:animateLayoutChanges set to true,
+        // adding this view is automatically animated.
+        mContainerView.addView(newView, 0);
+    }
+
+
+    /**
+     * A static list of item names.
+     */
+    private static final String[] ITEMS = new String[]{
+            "Apple", "Orange", "Juice", "Meat", "Water",
+            "Cheese", "Eggs", "Ice Cream", "Butter", "Bread",
+            "Nutella",
+    };
 
 
     @Override
@@ -43,4 +103,7 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
