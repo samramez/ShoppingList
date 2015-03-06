ea24514be14591854f586080a4f32f63fa3d6c98
Sam Ramezanli <samramezz@gmail.com>
2015-03-05 22:50:31 -0500

    FloatingActionButton now works

--- app/src/main/java/com/example/samramez/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/samramez/shoppinglist/MainActivity.java
@@ -1,99 +1,78 @@
 package com.example.samramez.shoppinglist;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.AbsListView;
+import android.widget.ListView;
+
+import com.melnykov.fab.FloatingActionButton;
+import com.melnykov.fab.ScrollDirectionListener;
 
 
 public class MainActivity extends Activity {
 
-    private Button addButton;
-    public EditText addItemEditText;
-    private ViewGroup mContainerView;
 
-
+    ListView listView;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //initiating page elements
-        addButton = (Button) findViewById(R.id.addButton);
-        addItemEditText = (EditText) findViewById(R.id.addItemEditText);
-        mContainerView = (ViewGroup) findViewById(R.id.container);
+        listView = (ListView) findViewById(android.R.id.list);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.attachToListView(listView);
 
-        addButton.setOnClickListener(new OnClickListener() {
+        fab.attachToListView(listView, new ScrollDirectionListener() {
             @Override
-            public void onClick(View view) {
-                findViewById(android.R.id.empty).setVisibility(View.GONE);
-                addItem();
+            public void onScrollDown() {
+                Log.d("ListViewFragment", "onScrollDown()");
+            }
+
+            @Override
+            public void onScrollUp() {
+                Log.d("ListViewFragment", "onScrollUp()");
+            }
+        }, new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                Log.d("ListViewFragment", "onScrollStateChanged()");
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                Log.d("ListViewFragment", "onScroll()");
             }
         });
-    }
 
-    // a method to inflate item to the main activity.
-    private void addItem() {
+        fab.setClickable(true);
+        fab.setOnTouchListener(new View.OnTouchListener() {
 
-        // Instantiate a new "row" view.
-        final ViewGroup newView = (ViewGroup) LayoutInflater.from(this).inflate(
-                R.layout.list_item_inflate, mContainerView, false);
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
 
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    Intent intent = new Intent(MainActivity.this, List.class);
+                    startActivity(intent);
+                    return true;
+                }
 
+                return true;
+            }
+        });
 
-        //getting the entered value from EditText
-        //String item = addItemEditText.getText().toString();
-
-        // Set the text in the new row to a random item.
-//        ((TextView) newView.findViewById(android.R.id.text1)).setText(
-//                ITEMS[(int) (Math.random() * ITEMS.length)]);
-
-        String item;
-        item = addItemEditText.getText().toString();
-        ((TextView) newView.findViewById(android.R.id.text1)).setText(item);
-
-
-                // Set a click listener for the "X" button in the row that will remove the row.
-                newView.findViewById(R.id.delete_button).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        // Remove the row from its parent (the container view).
-                        // Because mContainerView has android:animateLayoutChanges set to true,
-                        // this removal is automatically animated.
-                        mContainerView.removeView(newView);
-
-                        // If there are no rows remaining, show the empty view.
-                        if (mContainerView.getChildCount() == 0) {
-                            findViewById(android.R.id.empty).setVisibility(View.VISIBLE);
-                        }
-                    }
-                });
-
-        // Because mContainerView has android:animateLayoutChanges set to true,
-        // adding this view is automatically animated.
-        mContainerView.addView(newView, 0);
-
-        //clear the EditText for the next thing
-        addItemEditText.setText("");
     }
 
 
-    /**
-     * A static list of item names.
-     */
-    private static final String[] ITEMS = new String[]{
-            "Apple", "Orange", "Juice", "Meat", "Water",
-            "Cheese", "Eggs", "Ice Cream", "Butter", "Bread",
-            "Nutella",
-    };
+
+
 
 
     @Override
@@ -109,12 +88,13 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
-
-
 
 }
